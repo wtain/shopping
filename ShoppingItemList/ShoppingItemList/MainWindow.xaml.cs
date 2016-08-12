e4f745b2ec5ab1555364bf0a443989898839f4b0
wtain <ramiz.zeynalov@gmail.com>
2016-08-12 13:50:32 +0300

    ShoppingItemsList: Initial commit

---
+++ ShoppingItemList/ShoppingItemList/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace ShoppingItemList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        protected static IMongoClient _client;
+        protected static IMongoDatabase _database;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _client = new MongoClient();
+            _database = _client.GetDatabase("shopping");
+        }
+    }
+}
